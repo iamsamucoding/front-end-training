f09bf600987399f4bf37e9824272445ae4b3d0eb
Samuel Martins <sbm.martins@gmail.com>
2019-05-05 21:23:36 +0200

    JS Class 8: Filtrando uma tabela

---
+++ 2.1_JavaScript/projeto/js/codigos-das-aulas-anteriores/aula07_RemocaoDelegacaoAnimacao/form.js
@@ -0,0 +1,133 @@
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+
+botaoAdicionar.addEventListener("click", function(event){
+    event.preventDefault(); // previna/ignore o comportamento padrão do evento
+                            // no caso, o comportamento do evento de click em um botão
+
+    var form = document.querySelector("#form-adiciona");
+
+    // Extraindo informacoes do paciente do form
+    var paciente = obtemPacienteDoFormulario(form); // JS object
+    // Cria a tr e a td do paciente
+    var pacienteTr = montaTr(paciente);
+
+    var erros = validaPacientes(paciente); // [] ou uma lista/array de erros
+
+    if (erros.length > 0) { // se tem ao menos uma mensagem de erro
+        exibeMensagensDeErro(erros);
+        return;
+    }
+
+    var tabela = document.querySelector("#tabela-pacientes");
+    tabela.appendChild(pacienteTr);
+
+    // limpa o formulario após a submissão
+    form.reset();
+
+    // Com a propriedade innerHTML, podemos editar obter o conteúdo HTML (HTML interno) de um elemento.
+    // limpa as mensagens de erro
+    var mensagensErro = document.querySelector("#mensagens-erro");
+    mensagensErro.innerHTML = "";
+});
+
+
+function exibeMensagensDeErro(erros){
+    var ul = document.querySelector("#mensagens-erro");
+    ul.innerHTML = ""; // limpa as mensagens de erro
+
+    // Uma opcão de for
+    // for(var i = 0; i < erros.length ; i++){
+    //     var erro = erros[i];
+    // ...
+    // }
+
+    // pra cada elemento do array, execute uma função pra ele
+    // o item da iteração é passado por parâmetro na função
+    erros.forEach(function(erro) {
+        var li = document.createElement("li");
+        li.textContent = erro;
+        ul.appendChild(li);
+    });
+}
+
+
+
+function obtemPacienteDoFormulario(form) {
+    // criando um objecto em JavaScript
+    var paciente = {
+        nome: form.nome.value,
+        peso: form.peso.value,
+        altura: form.altura.value,
+        gordura: form.gordura.value,
+        imc: calculaImc(form.peso.value, form.altura.value) // isso poderia ser substituído pelo código abaixo
+    }
+
+    // Cria o atributo imc e armazena o valor resultante da função
+    // paciente.imc = calculaImc(paciente.peso, paciente.altura);
+
+    return paciente;
+}
+
+
+function montaTr(paciente) {
+    var pacienteTr = document.createElement("tr");
+    pacienteTr.classList.add("paciente"); // adicionando a classe do elemento
+
+    pacienteTr.appendChild(montaTd(paciente.nome, "info-nome"));
+    pacienteTr.appendChild(montaTd(paciente.peso, "info-peso"));
+    pacienteTr.appendChild(montaTd(paciente.altura, "info-peso"));
+    pacienteTr.appendChild(montaTd(paciente.gordura, "info-gordura"));
+    pacienteTr.appendChild(montaTd(paciente.imc, "info-imc"));
+
+    return pacienteTr;
+}
+
+function montaTd(dado, classe){
+    var td = document.createElement("td");
+    td.textContent = dado;
+    td.classList.add(classe);
+
+    return td;
+}
+
+
+function validaPeso(peso){
+    return (peso >= 0 && peso <= 1000);
+}
+
+function validaAltura(altura) {
+    return (altura >= 0 && altura <= 3.0);
+}
+
+// retorna uma lista com mensagens de erros
+function validaPacientes(paciente){
+    var erros = [];
+
+    if (paciente.nome.length == 0){
+        erros.push("O nome não pode ser em branco");
+    }
+
+    if (paciente.gordura.length == 0){
+        erros.push("A gordura não pode ser em branco");
+    }
+
+    if (paciente.peso.length == 0){
+        erros.push("O peso não pode ser em branco");
+    }
+
+    if (paciente.altura.length == 0){
+        erros.push("A altura não pode ser em branco");
+    }
+
+    if (!validaPeso(paciente.peso)){
+        erros.push("Peso é inválido");
+    }
+
+    if (!validaAltura(paciente.altura)){
+        erros.push("Altura é inválida");
+    }
+
+    return erros;
+}
+
+
---
+++ 2.1_JavaScript/projeto/js/filtra.js
@@ -0,0 +1,43 @@
+var campoFiltro = document.querySelector("#filtrar-tabela");
+
+
+// O evento input detectaa o evento de digitar (input de dados).
+// Toda vez que alguém inserir dados no campo, esse evento será disparado.
+campoFiltro.addEventListener("input", function(){
+    console.log(this.value); // this é o dono do evento (no caso, o <input>
+
+    var pacientes = document.querySelectorAll(".paciente");
+
+    // Cria um objecto RegExp para testar/filtar os elementos/linhas da tabela pelo
+    // nome que vai sendo digitado aos poucos no <input>.
+    // O primeiro campo é a expressão regular a ser testada (no caso, qualquer nome que tenha o texto
+    // digitado no <input> como parte/substring);
+    // O segundo campo "i" indica "case insensitive".
+    var expressao = new RegExp(this.value, "i");
+    
+
+    // se tem algum nome digitado, mostra apenas os nomes que começam com o que foi escrito, apagando o resto.
+    // Pra isso, adicionamos e removemos a classe ".invisivel" das linhas da tabela.
+    if (this.value.length > 0) {
+        for (var i = 0; i < pacientes.length; i++){
+            var paciente = pacientes[i];
+            var tdNome = paciente.querySelector(".info-nome");
+            var nome = tdNome.textContent;
+
+            // o metodo test testa o nome da linha atual da tabela com a expressão regular criada.
+            if (expressao.test(nome)) {
+                paciente.classList.remove("invisivel");
+            } else {
+                paciente.classList.add("invisivel");
+            }
+        }
+    }
+    // Mas, ao apagar o texto inteiro do <input>, volte a mostrar todas as linhas da tabela,
+    // ou seja, remova a class ".invisivel" de todos.
+    else {
+        for (var i = 0; i < pacientes.length; i++) {
+            var paciente = pacientes[i];
+            paciente.classList.remove("invisivel");
+        }
+    }
+});
